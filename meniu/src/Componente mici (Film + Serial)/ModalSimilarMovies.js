216b66c7321b1f5df322985f4e185d3e99689189
RotaruEmilianTeodor1086 <emilian.teodor18@gmail.com>
2021-07-16 22:45:07 +0300

    schimbari de desing + responsiveness

--- meniu/src/Componente mici (Film + Serial)/ModalSimilarMovies.js
+++ meniu/src/Componente mici (Film + Serial)/ModalSimilarMovies.js
@@ -11,7 +11,7 @@ const ModalSimilarMovies = ({id, title, poster_path, vote_average, release_date}
                 <div className="movie-header-similar-filme">
                     <img src={poster_path ? (IMG_API + poster_path) : 'https://images.unsplash.com/photo-1512070679279-8988d32161be?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=678&q=80'} alt={title}/>
                     <a style = {{position: 'absolute', top: 5, left: 17, fontFamily: 'poppins', marginLeft: '200px', color: 'black'}} href = {`https://www.themoviedb.org/movie/${id}`} target='_blank' data-toggle="tooltip" data-placement="top" title="Mai multe info"><SiThemoviedatabase className = 'FaInfoRecomandari'></SiThemoviedatabase></a>
-                    <span style = {{position: 'absolute', top: '4px', left: '4px', fontSize: '17px', fontFamily: 'Poppins', fontWeight: 'lighter'}} className="badge rounded-pill bg-danger">{vote_average}</span>
+                    <span style = {{position: 'absolute', top: '4px', left: '4px', fontSize: '17px', fontFamily: 'Poppins', fontWeight: 'lighter'}} className="badge rounded-pill bg-danger">{vote_average.toString().substr(0, 3)}</span>
                     <div className="movie-info-similar-filme">
                         <h3 style = {{fontSize: 15, color: 'black'}}>{title} ({release_date.substring(0,4)})</h3>
                     </div>
